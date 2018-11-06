77561c8f4e4ca0769f2250a0b48e88c7b6bf958f
mofaza <eliaslind91@gmail.com>
2018-11-06 11:28:41 +0100

    First part

---
+++ app/src/main/java/elias/lind/recipeapp/Recipe.java
@@ -0,0 +1,42 @@
+package elias.lind.recipeapp;
+
+import java.util.List;
+
+public class Recipe {
+    private String name;
+    private int minutes;
+    private List<String> ingredients;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
